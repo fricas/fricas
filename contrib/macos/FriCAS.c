7ed73b26b050a14acbf19cf673ea625ebeca8db3
Krystian Bacławski <krystian.baclawski@gmail.com>
2013-05-01 13:27:09 +0000

    Make application bundle for MacOSX.
    git-svn-id: https://fricas.svn.sourceforge.net/svnroot/fricas/trunk@1530 b0c55286-4f34-0410-a049-a1e7e93b0762

---
+++ contrib/macos/FriCAS.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char *argv[]) {
+  CFBundleRef bundle;
+  CFMutableStringRef path;
+  CFStringRef prefix;
+  CFStringRef resources;
+  CFStringEncoding encoding;
+ 
+  bundle = CFBundleGetMainBundle();
+  prefix = CFURLCopyFileSystemPath(
+               CFBundleCopyBundleURL(bundle),
+               kCFURLPOSIXPathStyle);
+  resources = CFURLCopyFileSystemPath(
+                  CFBundleCopyResourcesDirectoryURL(bundle),
+                  kCFURLPOSIXPathStyle);
+  encoding = CFStringGetSystemEncoding();
+
+  path = CFStringCreateMutable(kCFAllocatorDefault, 0);
+
+  CFStringAppend(path, prefix);
+  CFStringAppendCString(path, "/", encoding);
+  CFStringAppend(path, resources);
+
+  setenv("FRICAS_PREFIX", CFStringGetCStringPtr(path, encoding), 1);
+
+  system("/usr/X11/bin/xterm"
+         " -e \"${FRICAS_PREFIX}/bin/fricas\"");
+  return 0;
+}
