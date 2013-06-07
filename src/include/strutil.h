abaaa1438a178e204de50328ab5353836240c78d
Waldek Hebisch <hebisch@math.uni.wroc.pl>
2013-06-07 18:18:20 +0000

    Correct a typo.
    git-svn-id: https://fricas.svn.sourceforge.net/svnroot/fricas/trunk@1558 b0c55286-4f34-0410-a049-a1e7e93b0762

--- src/include/strutil.h
+++ src/include/strutil.h
@@ -6,28 +6,28 @@
 #define fricas_sprintf_to_buf1(buff, format, arg1) \
   do { \
      size_t fricas_sprintf_buffer_size = sizeof(buff); \
-     int fricas_sprintf_actial_size = \
+     int fricas_sprintf_actual_size = \
         snprintf(buff, fricas_sprintf_buffer_size, format, arg1); \
-     assert(fricas_sprintf_actial_size >= 0 && \
-            fricas_sprintf_actial_size < fricas_sprintf_buffer_size); \
+     assert(fricas_sprintf_actual_size >= 0 && \
+            fricas_sprintf_actual_size < fricas_sprintf_buffer_size); \
   } while(0)
 
 #define fricas_sprintf_to_buf2(buff, format, arg1, arg2) \
   do { \
      size_t fricas_sprintf_buffer_size = sizeof(buff); \
-     int fricas_sprintf_actial_size = \
+     int fricas_sprintf_actual_size = \
         snprintf(buff, fricas_sprintf_buffer_size, format, arg1, arg2); \
-     assert(fricas_sprintf_actial_size >= 0 && \
-            fricas_sprintf_actial_size < fricas_sprintf_buffer_size); \
+     assert(fricas_sprintf_actual_size >= 0 && \
+            fricas_sprintf_actual_size < fricas_sprintf_buffer_size); \
   } while(0)
 
 #define fricas_sprintf_to_buf3(buff, format, arg1, arg2, arg3) \
   do { \
      size_t fricas_sprintf_buffer_size = sizeof(buff); \
-     int fricas_sprintf_actial_size = \
+     int fricas_sprintf_actual_size = \
         snprintf(buff, fricas_sprintf_buffer_size, format, arg1, arg2, arg3); \
-     assert(fricas_sprintf_actial_size >= 0 && \
-            fricas_sprintf_actial_size < fricas_sprintf_buffer_size); \
+     assert(fricas_sprintf_actual_size >= 0 && \
+            fricas_sprintf_actual_size < fricas_sprintf_buffer_size); \
   } while(0)
 
 #endif
