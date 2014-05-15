6b9649bf8af93d2b1d00ec82f783ea9e86d241f1
Waldek Hebisch <hebisch@math.uni.wroc.pl>
2014-05-15 13:02:32 +0000

    Do not use notangle during build.
    git-svn-id: https://fricas.svn.sourceforge.net/svnroot/fricas/trunk@1763 b0c55286-4f34-0410-a049-a1e7e93b0762

--- config/var-def.mk
+++ config/var-def.mk
@@ -164,6 +164,7 @@ DOCUMENT=${axiom_build_document}
 ## -- Files generated for the build machine --
 ## -------------------------------------------
 axiom_build_document = @abs_top_builddir@/build/scripts/document
+unpack_file = $(fricas_src_srcdir)/scripts/unpack_file
 
 TANGLE = @NOTANGLE@
 WEAVE = @NOWEAVE@
