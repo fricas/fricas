fricas/fricas

commit 4d47c00545dc9aa82ae866adb46332a654348bc4

Use '|$spadroot|' instead of '(|getEnv| "FRICAS")'

src/interp/htcheck.boot
@@ -80,7 +80,7 @@ $primitiveHtCommands := '(
 
 buildHtMacroTable() ==
   $htMacroTable := MAKE_HASHTABLE('EQUAL)
-  fn := CONCAT(getEnv '"FRICAS", '"/share/hypertex/pages/util.ht")
+  fn := CONCAT($spadroot, '"/share/hypertex/pages/util.ht")
   if PROBE_-FILE(fn) then
     instream := MAKE_INSTREAM(fn)
     while not EOFP instream repeat

src/interp/br-data.boot
@@ -197,13 +197,13 @@ dbHasExamplePage conname ==
   sname    := STRINGIMAGE conname
   abb      := constructor? conname
   ucname   := UPCASE STRINGIMAGE abb
-  pathname :=STRCONC(getEnv '"FRICAS",'"/share/hypertex/pages/",ucname,'".ht")
+  pathname :=STRCONC($spadroot, '"/share/hypertex/pages/", ucname, '".ht")
   isExistingFile pathname => INTERN STRCONC(sname,'"XmpPage")
   nil
 
 dbReadComments(n) ==
   n = 0 => '""
-  instream := MAKE_INSTREAM(STRCONC(getEnv('"FRICAS"), '"/algebra/comdb.text"))
+  instream := MAKE_INSTREAM(STRCONC($spadroot, '"/algebra/comdb.text"))
   FILE_-POSITION(instream,n)
   line := read_line instream
   k := dbTickIndex(line,1,1)

src/interp/br-search.boot
@@ -205,7 +205,7 @@ isFilterDelimiter? c == MEMQ(c,$pmFilterDelimiters)
 
 grepSplit(lines,doc?) ==
   if doc? then
-    instream2 := OPEN STRCONC(getEnv '"FRICAS",'"/algebra/libdb.text")
+    instream2 := OPEN STRCONC($spadroot, '"/algebra/libdb.text")
   cons := atts := doms := nil
   while lines is [line, :lines] repeat
     if doc? then
@@ -879,14 +879,14 @@ dbGetCommentOrigin line ==
   line
 
 grepSource key ==
-  key = 'libdb   => STRCONC($SPADROOT,'"/algebra/libdb.text")
-  key = 'gloss   => STRCONC($SPADROOT,'"/algebra/glosskey.text")
+  key = 'libdb   => STRCONC($spadroot,'"/algebra/libdb.text")
+  key = 'gloss   => STRCONC($spadroot,'"/algebra/glosskey.text")
   key = $localLibdb => $localLibdb
   mkGrepTextfile
     MEMQ(key, '(_. a c d k o p x)) => 'libdb
     'comdb
 
-mkGrepTextfile s == STRCONC($SPADROOT,"/algebra/", STRINGIMAGE s, '".text")
+mkGrepTextfile s == STRCONC($spadroot,"/algebra/", STRINGIMAGE s, '".text")
 
 mkGrepFile s ==  --called to generate a path name for a temporary grep file
   prefix := '"/tmp/"

src/interp/daase.lisp
@@ -307,7 +307,7 @@ database.
  "call the aldor compiler"
  (#| system::system |#
    obey
-   (concatenate 'string (|getEnv| "FRICAS") "/compiler/bin/aldor "
+   (concatenate 'string |$spadroot| "/compiler/bin/aldor "
     flags " " file)))
 
 (defun resethashtables (display_messages)
@@ -412,7 +412,7 @@ database.
   |Integer| |List| |OutputForm|))
  (dolist (con constr)
   (let ((c (concatenate 'string
-             (|getEnv| "FRICAS") "/algebra/"
+             |$spadroot| "/algebra/"
              (string (getdatabase con 'abbreviation)) "." |$lisp_bin_filetype|)))
     (if display_messages
         (format t "   preloading ~a.." c))
@@ -490,7 +490,7 @@ database.
 ; system files is only the filename and extension. for user files it
 ; contains the full pathname. when the database is first opened the
 ; sourcefile slot contains system names. the lookup function
-; has to prefix the $spadroot information if the directory-namestring is
+; has to prefix the |$spadroot| information if the directory-namestring is
 ; null (we don't know the real root at database build time).
 ; a object-hash table is set up to look up nrlib and ao information.
 ; this slot is empty until a user does a )library call. we remember
@@ -663,7 +663,7 @@ database.
      (setf (get constructor 'abbreviationfor) nil)))))
 
 (defun getdatabase (constructor key)
- (declare (special $spadroot) (special |$miss|))
+ (declare (special |$spadroot|) (special |$miss|))
  (when (eq |$miss| t) (format t "getdatabase call: ~20a ~a~%" constructor key))
  (let (data table stream struct)
   (when (or (symbolp constructor)
@@ -795,7 +795,7 @@ database.
      (when (and data (string= (directory-namestring data) "")
              (string= (pathname-type data) "spad"))
       (setq data
-       (concatenate 'string $spadroot "/../../src/algebra/" data))))
+       (concatenate 'string |$spadroot| "/../../src/algebra/" data))))
     (asharp?                               ; is this asharp code?
      (if (consp data)
       (setq data (cdr data))
@@ -804,11 +804,11 @@ database.
      (if (consp data)
        (setq data
              (if (string= (directory-namestring (car data)) "")
-                 (concatenate 'string $spadroot "/algebra/" (car data)
+                 (concatenate 'string |$spadroot| "/algebra/" (car data)
                                         "." |$lisp_bin_filetype|)
                (car data)))
       (when (and data (string= (directory-namestring data) ""))
-       (setq data (concatenate 'string $spadroot "/algebra/" data
+       (setq data (concatenate 'string |$spadroot| "/algebra/" data
                                           "." |$lisp_bin_filetype|)))))))
   data))
 
@@ -1180,12 +1180,12 @@ database.
 
 (defun DaaseName (name)
  (let (daase filename)
-  (declare (special $spadroot))
+  (declare (special |$spadroot|))
   (if (setq daase (|getEnv| "DAASE"))
    (progn
     (setq filename  (concatenate 'string daase "/algebra/" name))
     (format t "   Using local database ~a.." filename))
-   (setq filename (concatenate 'string $spadroot "/algebra/" name)))
+   (setq filename (concatenate 'string |$spadroot| "/algebra/" name)))
   filename))
 
 ;; The compress database is special. It contains a list of symbols.
@@ -1426,7 +1426,7 @@ database.
 
 ;; following needs to happen inside restart since $FRICAS may change
 (defun |createInitializers2| ()
- (let ((asharprootlib (strconc (|getEnv| "FRICAS") "/aldor/lib/")))
+ (let ((asharprootlib (strconc |$spadroot| "/aldor/lib/")))
    (set-file-getter (strconc asharprootlib "runtime"))
    (set-file-getter (strconc asharprootlib "lang"))
    (set-file-getter (strconc asharprootlib "attrib"))

src/interp/ht-root.boot
@@ -130,7 +130,7 @@ htGlossPage(htPage,pattern,tryAgain?) ==
   grepForm := mkGrepPattern(filter,'none)
   $key: local := 'none
   results := applyGrep(grepForm,'gloss)
-  defstream := MAKE_INSTREAM(STRCONC(getEnv '"FRICAS",
+  defstream := MAKE_INSTREAM(STRCONC($spadroot,
                                      '"/algebra/glossdef.text"))
   lines := gatherGlossLines(results,defstream)
   heading :=

src/interp/i-syscmd.boot
@@ -706,9 +706,9 @@ withAsharpCmd args ==
 --% )copyright -- display copyright notice
 
 summary l ==
- OBEY STRCONC ('"cat ",getEnv('"FRICAS"),'"/lib/summary")
+ OBEY STRCONC ('"cat ", $spadroot, '"/lib/summary")
 copyright () ==
- OBEY STRCONC ('"cat ",getEnv('"FRICAS"),'"/lib/copyright")
+ OBEY STRCONC ('"cat ", $spadroot, '"/lib/copyright")
 
 --% )credits -- display credit list
 

src/interp/pathname.boot
@@ -61,4 +61,4 @@ isExistingFile f ==
 
 --% Scratchpad II File Name Functions
 
-isSystemDirectory dir == EVERY(function CHAR_=,$SPADROOT,dir)
+isSystemDirectory dir == EVERY(function CHAR_=,$spadroot,dir)

src/interp/util.lisp
@@ -39,7 +39,7 @@ functions we need to define here so they work and are available
 at load time.
 |#
 (in-package "BOOT")
-(export '($spadroot $directory-list reroot
+(export '(|$spadroot| $directory-list reroot
           make-absolute-filename |$defaultMsgDatabaseName|))
 
 ;;; Various lisps use different ``extensions'' on the filename to indicate
@@ -77,23 +77,23 @@ at load time.
 ;;; It is set up in the {\bf reroot} function.
 (defvar $library-directory-list ())
 
-;;; Prefix a filename with the {\bf $spadroot} variable.
+;;; Prefix a filename with the {\bf |$spadroot|} variable.
 (defun make-absolute-filename (name)
- (concatenate 'string $spadroot name))
+ (concatenate 'string |$spadroot| name))
 
 #|
 The reroot function is used to reset the important variables used by
 the system. In particular, these variables are sensitive to the
 {\bf FRICAS} shell variable. That variable is renamed internally to
-be {\bf \$spadroot}. The {\bf reroot} function will change the
+be {\bf |$spadroot|}. The {\bf reroot} function will change the
 system to use a new root directory and will have the same effect
 as changing the {\bf FRICAS} shell variable and rerunning the system
 from scratch.
 |#
-(defvar $spadroot "")
+(defvar |$spadroot| "")
 
 (defun reroot (dir)
-  (setq $spadroot dir)
+  (setq |$spadroot| dir)
   (setq $directory-list
    (mapcar #'make-absolute-filename $relative-directory-list))
   (setq $library-directory-list
@@ -212,7 +212,7 @@ After this function is called the image is clean and can be saved.
                 (if (and sym (boundp sym))
                     (setf (symbol-value sym) t)))
      (interpsys-image-init nil)
-     (format *standard-output* "spad = ~s~%" $spadroot)
+     (format *standard-output* "spad = ~s~%" |$spadroot|)
      (force-output  *standard-output*)
      (format *standard-output* "before fricas-restart~%")
      (force-output  *standard-output*)
@@ -250,7 +250,7 @@ After this function is called the image is clean and can be saved.
  (cond
   ((load "./exposed" :verbose nil :if-does-not-exist nil)
     '|done|)
-  ((load (concat (|getEnv| "FRICAS") "/algebra/exposed")
+  ((load (make-absolute-filename "/algebra/exposed")
      :verbose nil :if-does-not-exist nil)
    '|done|)
   (t '|failed|) ))
@@ -272,7 +272,7 @@ After this function is called the image is clean and can be saved.
     (if *fricas-load-libspad*
         (let ((spad-lib (make-absolute-filename "/lib/libspad.so")))
             (format t "Checking for foreign routines~%")
-            (format t "FRICAS=~S~%" $spadroot)
+            (format t "FRICAS=~S~%" |$spadroot|)
             (format t "spad-lib=~S~%" spad-lib)
             (if (|fricas_probe_file| spad-lib)
                 (progn

commit 2c2ce27b2e44321d8bc9eb79768fd4cb3421ee19

Fix loading of util.ht caused by 4d47c005
util.ht is read at each startup instead of compile time

src/doc/Makefile.in
@@ -255,7 +255,6 @@ GEN_HTFILES = xmpexp coverex gloss
 # The reason for this delayed execution is that generation of the .xpm
 # files in *.VIEW/ without the xvfb-run program will pop up image
 # windows in X and will make working during compilation impossible.
-# The intro.htex and util.ht files are not relevant for image generation.
 VIEWPORTFILES = coverex \
   $(shell grep -lRe 'graphpaste{\|spadgraph{' ${srcdir}/ht/* ${srcdir}/htex/* \
           | sed 's,.*/,,;s,\..*,,' \

src/interp/htcheck.boot
@@ -125,5 +125,3 @@ spadSysBranch(tree,arg) ==  --tree is (msg kind TREEorSomethingElse ...)
   kind = 'INTEGER  => INTEGERP arg
   kind = 'FUNCTION => atom arg
   systemError '"unknown tree branch"
-
-buildHtMacroTable()

src/interp/i-toplev.boot
@@ -94,6 +94,7 @@ interpsys_restart() ==
   loadExposureGroupData()
   initHist()
   initializeInterpreterFrameRing()
+  buildHtMacroTable()
 
   if $displayStartMsgs then spadStartUpMsgs()
   $currentLine := nil